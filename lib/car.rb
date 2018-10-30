a7cb77e7d3bd3579b3ef4af330c2b893849de9ad
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 18:10:16 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,8 @@
 require_relative "./vehicle.rb"
 
 class Car
-  attr_accessor :wheel_size, :wheel_num
+  attr_accessor :wheel_size
+  attr_reader :wheel_num
 
   def initialize(wheel_size, wheel_num)
     @wheel_size = wheel_size
